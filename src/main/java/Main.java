6ef73b41f51da213fa344365a31186f8d4b666ab
nati schmidt <nati.schmidt97@outlook.de>
2023-12-01 21:15:46 +0100

    testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
-
+import events.MyListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import javax.security.auth.login.LoginException;
 
 
@@ -9,8 +8,10 @@ public class Main
 {
     public static void main(String[] args) throws LoginException {
 
-        JDA bot = JDABuilder.createDefault("MTE3OTE0NzgwNjc5MTk2MjY4NA.GFygU1.XT9uhdb4ev9ZnU6b5R22QjXCAUk8hV9vz3kYPo")
-                .setActivity(Activity.playing("test"))
+        JDA bot = JDABuilder.createDefault
+
+                ("MTE3OTE0NzgwNjc5MTk2MjY4NA.GFygU1.XT9uhdb4ev9ZnU6b5R22QjXCAUk8hV9vz3kYPo")
+                .addEventListeners(new MyListener())
                 .build();
 
     }
